e7efabb84a5b4723f36735bba43018eabeedb3a3
wuallenwu <allenwu.12@outlook.com>
2024-05-29 05:19:58 +0000

    added stompy meshes and testenvironment

--- mani_skill/agents/robots/stompy/stompy.py
+++ mani_skill/agents/robots/stompy/stompy.py
@@ -2,7 +2,7 @@ import numpy as np
 import sapien
 from transforms3d import euler
 
-from mani_skill import ASSET_DIR
+from mani_skill import PACKAGE_ASSET_DIR
 from mani_skill.agents.base_agent import BaseAgent, Keyframe
 from mani_skill.agents.controllers import *
 from mani_skill.agents.registration import register_agent
@@ -12,7 +12,7 @@ from mani_skill.sensors.camera import CameraConfig
 @register_agent()  # uncomment this if you want to register the agent so you can instantiate it by ID when creating environments
 class Stompy(BaseAgent):
     uid = "stompy"
-    urdf_path = f"{ASSET_DIR}/robots/stompy/robot.urdf"
+    urdf_path = f"{PACKAGE_ASSET_DIR}/robots/stompy/robot.urdf"
     urdf_config = dict(
         _materials=dict(
             gripper=dict(static_friction=2.0, dynamic_friction=2.0, restitution=0.0)
---
+++ mani_skill/examples/demo_stompy.py
@@ -0,0 +1,313 @@
+import argparse
+
+import gymnasium as gym
+import numpy as np
+
+from mani_skill.envs.sapien_env import BaseEnv
+from mani_skill.utils.wrappers import RecordEpisode
+
+from typing import Any, Dict, Union
+import os
+import torch
+import torch.random
+from transforms3d.euler import euler2quat
+
+from mani_skill.agents.robots import Stompy
+from mani_skill.agents.base_agent import BaseAgent
+from mani_skill.agents.controllers import *
+
+from mani_skill.sensors.camera import CameraConfig
+from mani_skill.utils import common, sapien_utils
+from mani_skill.utils.building import actors
+from mani_skill.utils.registration import register_env
+from mani_skill.utils.scene_builder.table import TableSceneBuilder
+from mani_skill.utils.structs import Pose
+from mani_skill.utils.structs.types import Array, GPUMemoryConfig, SimConfig
+
+@register_env("Stompy-PushCube", max_episode_steps=50)
+class SPushCubeEnv(BaseEnv):
+    """
+    Task Description
+    ----------------
+    A simple task where the objective is to push and move a cube to a goal region in front of it
+
+    Randomizations
+    --------------
+    - the cube's xy position is randomized on top of a table in the region [0.1, 0.1] x [-0.1, -0.1]. It is placed flat on the table
+    - the target goal region is marked by a red/white circular target. The position of the target is fixed to be the cube xy position + [0.1 + goal_radius, 0]
+
+    Success Conditions
+    ------------------
+    - the cube's xy position is within goal_radius (default 0.1) of the target's xy position by euclidean distance.
+
+    Visualization: https://maniskill.readthedocs.io/en/latest/tasks/index.html#pushcube-v1
+    """
+
+    SUPPORTED_ROBOTS = ["stompy"]
+
+    # Specify some supported robot types
+    agent: Union[Stompy]
+
+    # set some commonly used values
+    goal_radius = 0.1
+    cube_half_size = 0.02
+
+    def __init__(self, *args, robot_uids="stompy", robot_init_qpos_noise=0.02, **kwargs):
+        # specifying robot_uids="panda" as the default means gym.make("PushCube-v1") will default to using the panda arm.
+        self.robot_init_qpos_noise = robot_init_qpos_noise
+        super().__init__(*args, robot_uids=robot_uids, **kwargs)
+
+    # Specify default simulation/gpu memory configurations to override any default values
+    @property
+    def _default_sim_config(self):
+        return SimConfig(
+            gpu_memory_cfg=GPUMemoryConfig(
+                found_lost_pairs_capacity=2**25, max_rigid_patch_count=2**18
+            )
+        )
+
+    @property
+    def _default_sensor_configs(self):
+        # registers one 128x128 camera looking at the robot, cube, and target
+        # a smaller sized camera will be lower quality, but render faster
+        pose = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
+        return [
+            CameraConfig(
+                "base_camera",
+                pose=pose,
+                width=128,
+                height=128,
+                fov=np.pi / 2,
+                near=0.01,
+                far=100,
+            )
+        ]
+
+    @property
+    def _default_human_render_camera_configs(self):
+        # registers a more high-definition (512x512) camera used just for rendering when render_mode="rgb_array" or calling env.render_rgb_array()
+        pose = sapien_utils.look_at([0.6, 0.7, 0.6], [0.0, 0.0, 0.35])
+        return CameraConfig(
+            "render_camera", pose=pose, width=512, height=512, fov=1, near=0.01, far=100
+        )
+
+    def _load_scene(self, options: dict):
+        # we use a prebuilt scene builder class that automatically loads in a floor and table.
+        self.table_scene = TableSceneBuilder(
+            env=self, robot_init_qpos_noise=self.robot_init_qpos_noise
+        )
+        self.table_scene.build()
+
+        # we then add the cube that we want to push and give it a color and size using a convenience build_cube function
+        # we specify the body_type to be "dynamic" as it should be able to move when touched by other objects / the robot
+        self.obj = actors.build_cube(
+            self.scene,
+            half_size=self.cube_half_size,
+            color=np.array([12, 42, 160, 255]) / 255,
+            name="cube",
+            body_type="dynamic",
+        )
+
+        # we also add in red/white target to visualize where we want the cube to be pushed to
+        # we specify add_collisions=False as we only use this as a visual for videos and do not want it to affect the actual physics
+        # we finally specify the body_type to be "kinematic" so that the object stays in place
+        self.goal_region = actors.build_red_white_target(
+            self.scene,
+            radius=self.goal_radius,
+            thickness=1e-5,
+            name="goal_region",
+            add_collision=False,
+            body_type="kinematic",
+        )
+
+        # optionally you can automatically hide some Actors from view by appending to the self._hidden_objects list. When visual observations
+        # are generated or env.render_sensors() is called or env.render() is called with render_mode="sensors", the actor will not show up.
+        # This is useful if you intend to add some visual goal sites as e.g. done in PickCube that aren't actually part of the task
+        # and are there just for generating evaluation videos.
+        # self._hidden_objects.append(self.goal_region)
+
+    def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
+        # use the torch.device context manager to automatically create tensors on CPU or CUDA depending on self.device, the device the environment runs on
+        with torch.device(self.device):
+            # the initialization functions where you as a user place all the objects and initialize their properties
+            # are designed to support partial resets, where you generate initial state for a subset of the environments.
+            # this is done by using the env_idx variable, which also tells you the batch size
+            b = len(env_idx)
+            # when using scene builders, you must always call .initialize on them so they can set the correct poses of objects in the prebuilt scene
+            # note that the table scene is built such that z=0 is the surface of the table.
+            self.table_scene.initialize(env_idx)
+
+            # here we write some randomization code that randomizes the x, y position of the cube we are pushing in the range [-0.1, -0.1] to [0.1, 0.1]
+            xyz = torch.zeros((b, 3))
+            xyz[..., :2] = torch.rand((b, 2)) * 0.2 - 0.1
+            xyz[..., 2] = self.cube_half_size
+            q = [1, 0, 0, 0]
+            # we can then create a pose object using Pose.create_from_pq to then set the cube pose with. Note that even though our quaternion
+            # is not batched, Pose.create_from_pq will automatically batch p or q accordingly
+            # furthermore, notice how here we do not even using env_idx as a variable to say set the pose for objects in desired
+            # environments. This is because internally any calls to set data on the GPU buffer (e.g. set_pose, set_linear_velocity etc.)
+            # automatically are masked so that you can only set data on objects in environments that are meant to be initialized
+            obj_pose = Pose.create_from_pq(p=xyz, q=q)
+            self.obj.set_pose(obj_pose)
+
+            # here we set the location of that red/white target (the goal region). In particular here, we set the position to be in front of the cube
+            # and we further rotate 90 degrees on the y-axis to make the target object face up
+            target_region_xyz = xyz + torch.tensor([0.1 + self.goal_radius, 0, 0])
+            # set a little bit above 0 so the target is sitting on the table
+            target_region_xyz[..., 2] = 1e-3
+            self.goal_region.set_pose(
+                Pose.create_from_pq(
+                    p=target_region_xyz,
+                    q=euler2quat(0, np.pi / 2, 0),
+                )
+            )
+
+    def evaluate(self):
+        # success is achieved when the cube's xy position on the table is within the
+        # goal region's area (a circle centered at the goal region's xy position)
+        is_obj_placed = (
+            torch.linalg.norm(
+                self.obj.pose.p[..., :2] - self.goal_region.pose.p[..., :2], axis=1
+            )
+            < self.goal_radius
+        )
+
+        return {
+            "success": is_obj_placed,
+        }
+
+    def _get_obs_extra(self, info: Dict):
+        # some useful observation info for solving the task includes the pose of the tcp (tool center point) which is the point between the
+        # grippers of the robot
+        obs = dict(
+            tcp_pose=self.agent.tcp.pose.raw_pose,
+        )
+        if self._obs_mode in ["state", "state_dict"]:
+            # if the observation mode is state/state_dict, we provide ground truth information about where the cube is.
+            # for visual observation modes one should rely on the sensed visual data to determine where the cube is
+            obs.update(
+                goal_pos=self.goal_region.pose.p,
+                obj_pose=self.obj.pose.raw_pose,
+            )
+        return obs
+
+    def compute_dense_reward(self, obs: Any, action: Array, info: Dict):
+        # We also create a pose marking where the robot should push the cube from that is easiest (pushing from behind the cube)
+        tcp_push_pose = Pose.create_from_pq(
+            p=self.obj.pose.p
+            + torch.tensor([-self.cube_half_size - 0.005, 0, 0], device=self.device)
+        )
+        tcp_to_push_pose = tcp_push_pose.p - self.agent.tcp.pose.p
+        tcp_to_push_pose_dist = torch.linalg.norm(tcp_to_push_pose, axis=1)
+        reaching_reward = 1 - torch.tanh(5 * tcp_to_push_pose_dist)
+        reward = reaching_reward
+
+        # compute a placement reward to encourage robot to move the cube to the center of the goal region
+        # we further multiply the place_reward by a mask reached so we only add the place reward if the robot has reached the desired push pose
+        # This reward design helps train RL agents faster by staging the reward out.
+        reached = tcp_to_push_pose_dist < 0.01
+        obj_to_goal_dist = torch.linalg.norm(
+            self.obj.pose.p[..., :2] - self.goal_region.pose.p[..., :2], axis=1
+        )
+        place_reward = 1 - torch.tanh(5 * obj_to_goal_dist)
+        reward += place_reward * reached
+
+        # assign rewards to parallel environments that achieved success to the maximum of 3.
+        reward[info["success"]] = 3
+        return reward
+
+    def compute_normalized_dense_reward(self, obs: Any, action: Array, info: Dict):
+        # this should be equal to compute_dense_reward / max possible reward
+        max_reward = 3.0
+        return self.compute_dense_reward(obs=obs, action=action, info=info) / max_reward
+    
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--env-id", type=str, default="Stompy-PushCube", help="The environment ID of the task you want to simulate")
+    parser.add_argument("-o", "--obs-mode", type=str, default="none")
+    parser.add_argument("-b", "--sim-backend", type=str, default="auto", help="Which simulation backend to use. Can be 'auto', 'cpu', 'gpu'")
+    parser.add_argument("--reward-mode", type=str)
+    parser.add_argument("-c", "--control-mode", type=str)
+    parser.add_argument("--render-mode", type=str, default="rgb_array")
+    parser.add_argument("--shader", default="default", type=str, help="Change shader used for rendering. Default is 'default' which is very fast. Can also be 'rt' for ray tracing and generating photo-realistic renders. Can also be 'rt-fast' for a faster but lower quality ray-traced renderer")
+    parser.add_argument("--record-dir", type=str)
+    parser.add_argument("-p", "--pause", action="store_true", help="If using human render mode, auto pauses the simulation upon loading")
+    parser.add_argument("--quiet", action="store_true", help="Disable verbose output.")
+    parser.add_argument(
+        "-s",
+        "--seed",
+        type=int,
+        help="Seed the random actions and simulator. Default is no seed",
+    )
+    args, opts = parser.parse_known_args(args)
+
+    # Parse env kwargs
+    if not args.quiet:
+        print("opts:", opts)
+    eval_str = lambda x: eval(x[1:]) if x.startswith("@") else x
+    env_kwargs = dict((x, eval_str(y)) for x, y in zip(opts[0::2], opts[1::2]))
+    if not args.quiet:
+        print("env_kwargs:", env_kwargs)
+    args.env_kwargs = env_kwargs
+
+    return args
+
+
+def main(args):
+    np.set_printoptions(suppress=True, precision=3)
+    verbose = not args.quiet
+    if args.seed is not None:
+        np.random.seed(args.seed)
+    env: BaseEnv = gym.make(
+        args.env_id,
+        obs_mode=args.obs_mode,
+        reward_mode=args.reward_mode,
+        control_mode=args.control_mode,
+        render_mode=args.render_mode,
+        shader_dir=args.shader,
+        sim_backend=args.sim_backend,
+        **args.env_kwargs
+    )
+
+    record_dir = args.record_dir
+    if record_dir:
+        record_dir = record_dir.format(env_id=args.env_id)
+        env = RecordEpisode(env, record_dir, info_on_video=True)
+
+    if verbose:
+        print("Observation space", env.observation_space)
+        print("Action space", env.action_space)
+        print("Control mode", env.unwrapped.control_mode)
+        print("Reward mode", env.unwrapped.reward_mode)
+
+    obs, _ = env.reset(seed=args.seed)
+    env.action_space.seed(args.seed)
+    if args.render_mode is not None:
+        viewer = env.render()
+        viewer.paused = args.pause
+        env.render()
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        if verbose:
+            print("reward", reward)
+            print("terminated", terminated)
+            print("truncated", truncated)
+            print("info", info)
+
+        if args.render_mode is not None:
+            env.render()
+
+        if args.render_mode is None or args.render_mode != "human":
+            if terminated or truncated:
+                break
+    env.close()
+
+    if record_dir:
+        print(f"Saving video to {record_dir}")
+
+
+if __name__ == "__main__":
+    main(parse_args())
